8054dcc7d9c040a968cfd0d1385eb20605816fc7
urongLee <71316796+urongLee@users.noreply.github.com>
2020-09-28 16:11:35 +0900

    Add files via upload

---
+++ C_CheckEdit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MES_Pro
+{
+    public class C_CheckEdit : DevExpress.XtraEditors.CheckEdit
+    {
+        public C_CheckEdit()
+        {
+
+        }
+    }
+}
